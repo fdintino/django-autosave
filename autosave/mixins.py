dd503036fd6b2ea2fe72e1acff0e19c4871bf070
Frankie Dintino <fdintino@theatlantic.com>
2013-06-13 23:56:15 -0400

    Misc fixes and code style changes

--- autosave/mixins.py
+++ autosave/mixins.py
@@ -1,12 +1,26 @@
+import re
 import copy
 import time
-import simplejson as json
+import json
+import functools
+import textwrap
+from datetime import datetime
+
 from django.contrib import messages
-from django.conf.urls.defaults import url, patterns
+from django.contrib.admin.util import unquote
 from django.conf import settings
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
-from django.core.exceptions import ImproperlyConfigured
+from django.core.urlresolvers import reverse
+from django.core.exceptions import ImproperlyConfigured, PermissionDenied
+from django.db.models.fields import FieldDoesNotExist
+from django.http import HttpResponse, Http404
+from django.utils.encoding import force_unicode
+# from django.utils.functional import curry
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext as _
+from django.views.decorators.csrf import csrf_exempt
+#from django.shortcuts import get_object_or_404
+
 
 class AdminAutoSaveMixin(object):
 
@@ -30,74 +44,183 @@ class AdminAutoSaveMixin(object):
 
         return Form
 
+    @csrf_exempt
+    def autosave_js(self, request, object_id, extra_context=None):
+        opts = self.model._meta
+        info = (opts.app_label, opts.module_name)
 
-    def last_updated(self, request, obj_id):
-        """
-        Simple JSON view to get the last updated time in both ISO and epoch.
-        Is based on the admin's "last_updated_field".
+        obj = None
+        updated = None
 
-        class MyAdmin(admin.ModelAdmin):
-            # stuff
-
-            autosave_last_modified_field = 'last_modified'
-
-        """
-        admin_class_name = ".".join([self.__module__, self.__class__.__name__]) # Used in error messages
-        try:
-            obj = get_object_or_404(self.model, id=obj_id)
-        except ValueError:
-            # If the object id is something like 'add', return null values.
-            output = {
-                'last_updated': None,
-                'last_updated_epoch': None,
-            }
-            output = json.dumps(output)
-            return HttpResponse(output, mimetype="application/json")
-
-
-        # Break if the admin doesn't have a 'autosave_last_modified_field' property
+        # Raise exception if the admin doesn't have a 'autosave_last_modified_field' property
         if not self.autosave_last_modified_field:
-            error_message = """Autosave isn't set up correctly. On {0}, you should
-            set a property called 'autosave_last_modified_field' to the name of your model's
-            last updated datetime field. """.format(admin_class_name)
-            raise ImproperlyConfigured(error_message)
+            raise ImproperlyConfigured((
+                u"Autosave is not configured correctly. %(cls_name)s "
+                u"is missing property 'autosave_last_modified_field', which "
+                u"should be set to the model's last updated datetime field.") % {
+                    'cls_name': ".".join([self.__module__, self.__class__.__name__]),
+                })
 
-        updated = getattr(obj, self.autosave_last_modified_field, None)
-        # Break if the field doesn't exist
-        if not updated:
-            error_message = """Autosave isn't set up correctly. {admin}.autosave_last_modified_field is
-            set to '{field}', which isn't a valid field on the model. """.format(
-                admin=admin_class_name,
-                field=self.autosave_last_modified_field,
-                )
-            raise ImproperlyConfigured(error_message)
+        # Raise exception if self.autosave_last_modified_field is not set
+        try:
+            opts.get_field_by_name(self.autosave_last_modified_field)
+        except FieldDoesNotExist:
+            raise
 
-        output = {
-            'last_updated': updated.isoformat(),
-            'last_updated_epoch': time.mktime(updated.timetuple()),
+        if not object_id:
+            autosave_url = reverse("admin:%s_%s_add" % info)
+        else:
+            autosave_url = reverse("admin:%s_%s_change" % info, args=[object_id])
+            try:
+                obj = self.get_object(request, unquote(object_id))
+            except (ValueError, self.model.DoesNotExist):
+                raise Http404(_('%(name)s object with primary key %(key)r does not exist.') % {
+                    'name': force_unicode(opts.verbose_name),
+                    'key': escape(object_id),
+                })
+            else:
+                updated = getattr(obj, self.autosave_last_modified_field, None)
+
+        if obj and not self.has_change_permission(request, obj):
+            raise PermissionDenied
+        elif not obj and not self.has_add_permission(request):
+            raise PermissionDenied
+
+        js_vars = {
+            'autosave_url': autosave_url,
+            'server_time_epoch': time.mktime(datetime.now().timetuple()),
+            'last_updated_epoch': time.mktime(updated.timetuple()) if updated else None,
+            'is_recovered_autosave': bool(request.GET.get('is_recovered')),
         }
-        output = json.dumps(output)
-        return HttpResponse(output, mimetype="application/json")
 
+        response_js = textwrap.dedent("""
+            var DjangoAutosave = (typeof window.DjangoAutosave != 'undefined')
+                               ? DjangoAutosave
+                               : {{}};
+            DjangoAutosave.config = (function() {{
+                var config = {config_data};
+                config.client_time_epoch = Math.round((new Date()).getTime()/1000, 0);
+                config.client_time_offset = config.client_time_epoch - config.server_time_epoch;
+                return config;
+            }})();
+        """).strip().format(config_data=json.dumps(js_vars, indent=4, sort_keys=True))
+        # config_data = json.dumps(js_vars, indent=4, sort_keys=True)
+        # json_output = response_js.format(config_data=config_data)
+        # response_js = textwrap.dedent(json_output).strip()
+        return HttpResponse(response_js, mimetype='application/x-javascript')
+
+    # def autosave_js(self, request, obj_id):
+    #     """
+    #     Simple JSON view to get the last updated time in both ISO and epoch.
+    #     Is based on the admin's "last_updated_field".
+    #
+    #     class MyAdmin(admin.ModelAdmin):
+    #         # stuff
+    #
+    #         autosave_last_modified_field = 'last_modified'
+    #
+    #     """
+    #     # Used in error messages
+    #     cls_name = ".".join([self.__module__, self.__class__.__name__])
+    #
+    #     try:
+    #         obj = get_object_or_404(self.model, id=obj_id)
+    #     except ValueError:
+    #         # If the object id is something like 'add', return null values.
+    #         output = {
+    #             'last_updated': None,
+    #             'last_updated_epoch': None,
+    #         }
+    #         output = json.dumps(output)
+    #         return HttpResponse(output, mimetype="application/json")
+    #
+    #
+    #     # Raise exception if the admin doesn't have 'autosave_last_modified_field' defined
+    #     if not self.autosave_last_modified_field:
+    #         error_message = """Autosave isn't set up correctly. On {0}, you should
+    #         set a property called 'autosave_last_modified_field' to the name of your model's
+    #         last updated datetime field. """.format(admin_class_name)
+    #         raise ImproperlyConfigured(error_message)
+    #
+    #     # Raise exception if the field doesn't exist
+    #     try:
+    #         self.model.get_field_by_name(self.autosave_last_modified_field)
+    #     except FieldDoesNotExist:
+    #         raise
+    #
+    #     updated = getattr(obj, self.autosave_last_modified_field, None)
+    #
+    #     if not updated:
+    #         error_message = """Autosave isn't set up correctly. {admin}.autosave_last_modified_field is
+    #         set to '{field}', which isn't a valid field on the model. """.format(
+    #             admin=admin_class_name,
+    #             field=self.autosave_last_modified_field,
+    #             )
+    #         raise ImproperlyConfigured(error_message)
+    #
+    #     output = {
+    #         'last_updated': updated.isoformat(),
+    #         'last_updated_epoch': time.mktime(updated.timetuple()),
+    #     }
+    #     output = json.dumps(output)
+    #     return HttpResponse(output, mimetype="application/json")
 
     def get_urls(self):
         """Adds a last-modified checker to the admin urls."""
+        try:
+            from django.conf.urls.defaults import patterns, url
+        except ImportError:
+            from django.conf.urls import patterns, url
 
-        urls = super(AdminAutoSaveMixin, self).get_urls()
-        extra_urls = patterns('',
-            # This has to be \w because if it's not, parameters following the obj_id will be
-            # caught up in the regular change_view url pattern, and 500.
-            url(r'^(?P<obj_id>\w+)/last-modified/$', self.last_updated),
-        )
-        return extra_urls + urls
+        opts = self.model._meta
+        info = (opts.app_label, opts.module_name)
+
+        # Use admin_site.admin_view to add permission checking
+        def wrap(view):
+            def wrapper(*args, **kwargs):
+                return self.admin_site.admin_view(view)(*args, **kwargs)
+            return functools.update_wrapper(wrapper, view)
+
+        # This has to be \w because if it's not, parameters following the obj_id will be
+        # caught up in the regular change_view url pattern, and 500.
+        urlpatterns = patterns('',
+            url(r'^(.+)/autosave_variables\.js',
+                wrap(self.autosave_js),
+                name="%s_%s_autosave_js" % info),)
+        urlpatterns += super(AdminAutoSaveMixin, self).get_urls()
+        return urlpatterns
 
     @property
     def media(self):
-        base_media = super(AdminAutoSaveMixin, self).media
-        base_media.add_js((
+        opts = self.model._meta
+        info = (opts.app_label, opts.module_name)
+
+        media = super(AdminAutoSaveMixin, self).media
+        media.add_js((
+            # We call admin:%(app_label)s_%(model)s_autosave_js with 0 (the pk)
+            # with the intention of doing a string replace on the url in
+            # render_change_form(), where we know what the primary key is.
+            #
+            # This is hacky, but necessary since the add_view does not have a
+            # primary key, given that the object has not yet been saved.
+            reverse('admin:%s_%s_autosave_js' % info, args=[0]),
             "%sautosave/js/autosave.js" % settings.STATIC_URL,
         ))
-        base_media.add_css({
-            'all': {},
-        })
-        return base_media
+        return media
+
+    def render_change_form(self, request, context, add=False, obj=None, **kwargs):
+        obj_pk = getattr(obj, 'pk', None)
+        if not add and obj_pk:
+            media = context.pop('media', None)
+            if media:
+                get_params = u''
+                if 'is_retrieved_from_autosave' in request.POST:
+                    get_params = u'?is_recovered=1'
+                # This is our hacky string-replacement, described more fully
+                # in the comments for the `media` @property
+                media = re.sub(r'/0/(autosave_variables\.js)',
+                    r'/%d/\1%s' % (obj_pk, get_params),
+                    unicode(media))
+                context['media'] = mark_safe(media)
+        return super(AdminAutoSaveMixin, self).render_change_form(
+                request, context, add=add, obj=obj, **kwargs)
--- autosave/static/autosave/js/autosave.js
+++ autosave/static/autosave/js/autosave.js
@@ -1,57 +1,61 @@
+var DjangoAutosave = (window.DjangoAutosave) ? DjangoAutosave : {};
+
 (function($) {
 
-    window.Autosave = {};
-
-    $(document).on('ready', function() {
-        Autosave.setUp(); 
+    $(document).ready(function() {
+        DjangoAutosave.setUp();
     });
 
     $(document).on('click', '[href=#ignore-autosaved]', function(e) {
         // Clicking this should remove the banner and start autosaving again, replacing
         // the old version.
-        var $btn = $(e.target);
-        var $note = $btn.closest('li');
-        $note.fadeOut('fast');
-        window.setInterval(Autosave.save, 5000);
+        e.preventDefault();
+        $(e.target).closest('li').fadeOut('fast');
+        DjangoAutosave.save();
+        window.setInterval(DjangoAutosave.save, 5000);
     });
 
     $(document).on('click', '[href=#revert-to-autosaved]', function(e) {
         // Regenerates the form to submit old data, and posts it.
-        
+        e.preventDefault();
         // Handle banner
         var $btn = $(e.target);
         var $banner = $btn.closest('p');
         $banner.text("Reverting to your saved version. Be right back...");
         
         // Generate new form data
-        var form = $('form');
-        form.find('input', 'textarea', '[name]').prop('disabled',true); // Clear the existing form
-        var data = JSON.parse(Autosave.retrieve()[0]);
-        
-        $.each(data, function(i, obj) {
-            var input = $('<input type="hidden" />')[0];
-            input.name = obj.name;
-            input.value = obj.value;
-            $('form').append(input);
+        var $form = $('form');
+        // Disable the existing form
+        $form.find(':input:not([name="csrfmiddlewaretoken"])').prop('disabled', true);
+        var data = DjangoAutosave.retrieve()[0];
+
+        $.each(data, function(i, attributes) {
+            $('<input type="hidden" />').attr(attributes).appendTo($form);
         });
 
         // The CSRF token can change and cause 403's. Always use the current one.
-        document.getElementsByName('csrfmiddlewaretoken')[0].value = Autosave.csrf_token;
-
+        if (DjangoAutosave.csrf_token) {
+            $(':input[name="csrfmiddlewaretoken"]').val(DjangoAutosave.csrf_token);
+        }
         function addAutoSaveRetrieveField() {
             // This adds an element to the page that tells Django forms
             // to deliberately fail validation, and return the autosaved contents.
             var input = $('<input type="hidden" name="is_retrieved_from_autosave" value="1" />');
             $('form').append(input);
         }
-        addAutoSaveRetrieveField();
-        form.submit();
 
+        // This adds an element to the page that tells Django forms
+        // to deliberately fail validation, and return the autosaved contents.
+        $form.append($('<input type="hidden" name="is_retrieved_from_autosave" value="1"/>'));
+        $form.submit();
     });
 
+    DjangoAutosave.setUp = function() {
+        if (typeof DjangoAutosave.config != 'object') {
+            return false;
+        }
 
-    Autosave.setUp = function() {
-        
+
         function pageIsChangeListView(){
             return $('#changelist-form').length == 1;
         }
@@ -60,115 +64,116 @@
             return false;
         }
 
-        Autosave.csrf_token = document.getElementsByName('csrfmiddlewaretoken')[0].value;
-        Autosave.timestamp = $.get('last-modified/', function(data) { // Get the last updated value from the server
-            if (data.last_updated_epoch === null){
-                return false; // No date means this object doesn't exist yet.
-            }
-            var last_updated = parseInt(data.last_updated_epoch, 0) + 15; // An arbitrary margin of error to deal with clock sync
-            var last_autosaved = parseInt(Autosave.retrieve()[1], 0);
+        DjangoAutosave.csrf_token = $('[name="csrfmiddlewaretoken"]').val();
 
-            // If last_updated is more recent, than this story was probably edited by someone else/another device.
-            // If the content is not different, the user probably just closed a window or went to get coffee and close a tab,
-            // but had already saved their work.
-            if ( last_autosaved > last_updated && Autosave.contentIsDifferent() ) {
-                // Suggest revert
-                Autosave.suggestRevert(last_autosaved);
-            } else {
-                // Start Saving Again
-                window.setInterval(Autosave.save, 5000);
-            }
-        });
+        var data = DjangoAutosave.config;
+
+        if (data.last_updated_epoch === null) {
+            // No date means this object doesn't exist yet.
+            return false;
+        }
+        // An arbitrary margin of error to deal with clock sync
+        var last_updated = parseInt(data.last_updated_epoch, 10) + data.client_time_offset + 15;
+        var last_autosaved = DjangoAutosave.retrieve()[1];
+
+        // If last_updated is more recent, than this story was probably edited by someone else/another device.
+        // If the content is not different, the user probably just closed a window or went to get coffee and close a tab,
+        // but had already saved their work.
+        if (!data.is_recovered_autosave && last_autosaved > last_updated && DjangoAutosave.contentIsDifferent()) {
+            // Suggest revert
+            DjangoAutosave.suggestRevert(last_autosaved);
+        } else {
+            // Start Saving Again
+            window.setInterval(DjangoAutosave.save, 5000);
+        }
     };
 
 
-    Autosave.contentIsDifferent = function() {
+    DjangoAutosave.contentIsDifferent = function() {
         // Determines if the autosaved data is different than the current version.
 
-        var saved = Autosave.retrieve()[0];
-        var current = Autosave.captureForm();
+        var saved = DjangoAutosave.retrieve()[0];
+        var current = DjangoAutosave.captureForm();
 
-        // Parse and compare each field
-        saved = JSON.parse(saved);
-        current = JSON.parse(current);
-        
-        // If they're not even the same length, they're different.       
+        // If they're not even the same length, they're different.
         if (saved.length !== current.length) {
             return true;
         }
         for (var i = saved.length - 1; i >= 0; i--) {
-            if(saved[i].value !== current[i].value && saved[i].name !== 'csrfmiddlewaretoken' ){
-                return true; // The values for non-ignored fields should be identical
+            // Skip comparison of the csrfmiddlewaretoken value
+            if (saved[i].name === 'csrfmiddlewaretoken') { continue; }
+            if (saved[i].value !== current[i].value) {
+                // The values for fields should be identical
+                return true;
             }
         }
         return false;
     };
 
-    function now() {
-        // This is slightly ridiculous because javascript's epoch time is
-        // in milliseconds by default. We need seconds.
-        return Math.round((new Date).getTime()/1000,0);
-    }
-
-    Autosave.suggestRevert = function(last_autosaved) {
+    DjangoAutosave.suggestRevert = function(last_autosaved) {
         var msg = [
             "It looks like you have a more recent version autosaved at ",
             Date(last_autosaved).toLocaleString(),
             ". <a href='#revert-to-autosaved'>Revert to that</a> or",
             " <a href='#ignore-autosaved'>continue with this version</a>?"
         ].join('');
-        var $alert = $('<li />');
-        $alert.addClass('warning');
-        $alert.hide();
-        $alert.html(msg);
+        var $alert = $('<li class="warning"/>').hide().html(msg);
+        // $alert.hide();
+        // $alert.html(msg);
 
-        var $messagelist = $('.messagelist');
-        var $container = $('#content, #content-inner'); // Support both Admin and Grapelli
-        if ($messagelist.length === 0) { 
+        // 'grp-' prefix to support both Admin and Grapelli 2.4
+        var $messagelist = $('.messagelist, .grp-messagelist');
+        var $container = $('#content, #content-inner');
+        if (!$messagelist.length) {
             // Put messagelist in place if it's not already there
-            $messagelist = $('<ul />');
-            $messagelist.addClass('messagelist grp-messagelist');
-            $container.prepend($messagelist);
+            $messagelist = $('<ul class="messagelist grp-messagelist"/>').prependTo($container);
         }
-
         $messagelist.append($alert);
         $alert.fadeIn();
     };
 
-    Autosave.getFormName = function() {
+    DjangoAutosave.getFormName = function() {
         // Key names are unique to the page/uri
         return "autosaved_form.data:" + window.location.pathname;
     };
-    Autosave.getTimeStampName = function() {
+    DjangoAutosave.getTimeStampName = function() {
         // Key names are unique to the page/uri
         return "autosaved_form.timestamp:" + window.location.pathname;
     };
     
-    Autosave.captureForm = function() {
-
-        var form = $('form');
-        var fields = $('form').find('textarea, [name][value]'); // Textareas don't have a value attr, need to be special
+    DjangoAutosave.captureForm = function() {
+        var $form = $('form');
+        var fields = $form.find(':input:not([name="csrfmiddlewaretoken"])');
         field_list = [];
-        var field;
+        var $field;
         for (var i = fields.length - 1; i >= 0; i--) {
-            field = fields[i];
-            field_list.push({ 'name': field.name, 'value': $(field).val() });
+            $field = $(fields[i]);
+            var name = $field.attr('name');
+            if (name) {
+                field_list.push({ 'name': name, 'value': $field.val() });
+            }
             // Val has to come from JQuery because CKeditor hooks it's update function in there.
         }
-        return JSON.stringify(field_list);
+        return field_list;
+        // return JSON.stringify(field_list);
     };
 
-    Autosave.save = function() {
-        var data = Autosave.captureForm();
-        localStorage.setItem(Autosave.getFormName(), data);
-        localStorage.setItem(Autosave.getTimeStampName(), now());
+    DjangoAutosave.save = function() {
+        var data = DjangoAutosave.captureForm();
+        localStorage.setItem(DjangoAutosave.getFormName(), JSON.stringify(data));
+        localStorage.setItem(DjangoAutosave.getTimeStampName(), Math.round((new Date()).getTime()/1000, 0));
     };
 
-    Autosave.retrieve = function() {
+    DjangoAutosave.retrieve = function() {
         // Get what's in storage
-        var data = localStorage.getItem(Autosave.getFormName());
-        var timestamp = localStorage.getItem(Autosave.getTimeStampName());
-        return [data, timestamp];
+        var data = localStorage.getItem(DjangoAutosave.getFormName());
+        var timestamp = localStorage.getItem(DjangoAutosave.getTimeStampName());
+        return [$.parseJSON(data), parseInt(timestamp, 10) || null];
+    };
+
+    DjangoAutosave.clear = function() {
+        localStorage.removeItem(DjangoAutosave.getFormName());
+        localStorage.removeItem(DjangoAutosave.getTimeStampName());
     };
 
 })(django.jQuery); // Must use Django jQuery because Django-CKEditor modifies it.
